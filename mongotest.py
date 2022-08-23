54e88d837b6d82c0689a2cb1f5ba2a56501ae7d2
stealthsuman <sumanvh007@gmail.com>
2022-08-23 19:47:14 +0530

    this is my first time pushing gitcode

---
+++ mongotest.py
@@ -0,0 +1,8 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://suman:suman123@cluster0.suttp1v.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d={"name":"suman","mail":"sumanvh007@gmail.com","surname":"vh"}
+db1=client["mangotest"]
+coll=db1.test
+coll.insert_one(d)
